423d0c68e009da4391abc99e4a7aeb1335ea2152
vivinvinh212 <83176944+vivinvinh212@users.noreply.github.com>
2022-09-05 00:25:38 +0700

    update oracle

--- packages/hardhat/contracts/Oracle.sol
+++ packages/hardhat/contracts/Oracle.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Oracle {
     IERC20 public WBTC;
-    IERC20 public ETH;
+    AggregatorV3Interface internal ETH;
     AggregatorV3Interface internal reservesWBTC;
 
     /**
@@ -16,49 +16,54 @@ contract Oracle {
      */
     constructor() {
         WBTC = IERC20(0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599);
-        ETH = IERC20(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419)
+        ETH = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
         reservesWBTC = AggregatorV3Interface(
             0xB622b7D6d9131cF6A1230EBa91E5da58dbea6F59
         );
     }
-}
 
-function getETHLatestPrice() public view returns (int) {
+    function getETHLatestPrice() public view returns (int) {
         (
-            /*uint80 roundID*/,
-            int price,
-            /*uint startedAt*/,
-            /*uint timeStamp*/,
-            /*uint80 answeredInRound*/
+            ,
+            /*uint80 roundID*/
+            int price, /*uint startedAt*/ /*uint timeStamp*/ /*uint80 answeredInRound*/
+            ,
+            ,
+
         ) = ETH.latestRoundData();
         return price;
     }
 
-//Returns the latest Supply info
-function getWBTCSupply() public view returns (uint256) {
-    return WBTC.totalSupply();
-}
+    //Returns the latest Supply info
+    function getWBTCSupply() public view returns (uint256) {
+        return WBTC.totalSupply();
+    }
 
-//Returns the latest Reserves info
-function getWBTCLatestReserves() public view returns (int) {
-    (
-        ,
-        /* uint80 roundID */
-        int answer, /* uint startedAt */
-        ,
-        ,
+    //Returns the latest Reserves info
+    function getWBTCLatestReserves() public view returns (int) {
+        (
+            ,
+            /* uint80 roundID */
+            int answer, /* uint startedAt */ /* uint updatedAt */ /* uint80 answeredInRound */
+            ,
+            ,
 
-    ) = /* uint updatedAt */
-        /* uint80 answeredInRound */
-        reservesWBTC.latestRoundData();
-    return answer;
-}
+        ) = reservesWBTC.latestRoundData();
+        return answer;
+    }
 
-//Determines if supply has exceeded reserves
-function checkWBTCReserves() public view returns (bool) {
-    return getWBTCLatestReserves() >= int(getWBTCSupply());
-}
+    //Determines if supply has exceeded reserves
+    function checkWBTCReserves() public view returns (bool) {
+        return getWBTCLatestReserves() >= int(getWBTCSupply());
+    }
 
-function checkETHPrice(uint upperThreshold, uint lowerThreshold) public view returns (bool){
-    return getETHLatestPrice() < upperThreshold && getETHLatestPrice() > lowerThreshold;
+    function checkETHPrice(uint upperThreshold, uint lowerThreshold)
+        public
+        view
+        returns (bool)
+    {
+        return
+            getETHLatestPrice() < int(upperThreshold) &&
+            getETHLatestPrice() > int(lowerThreshold);
+    }
 }
