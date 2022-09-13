c7b058a8ad268779e4e6e869ce87a1968b7a02fb
vivinvinh212 <83176944+vivinvinh212@users.noreply.github.com>
2022-09-13 19:06:26 +0700

    update contract

--- packages/hardhat/contracts/MultiSigWalletTest.sol
+++ packages/hardhat/contracts/MultiSigWalletTest.sol
@@ -1,309 +1,323 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity >=0.8.0 <0.9.0;
-// pragma experimental ABIEncoderV2;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+pragma experimental ABIEncoderV2;
 
-// import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-// import "./MultiSigFactory.sol";
-// import "./Oracle.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "./MultiSigFactory.sol";
+import "./Oracle.sol";
 
-// //custom errors
-// error DUPLICATE_OR_UNORDERED_SIGNATURES();
-// error INVALID_OWNER();
-// error INVALID_SIGNER();
-// error INVALID_SIGNATURES_REQUIRED();
-// error INSUFFICIENT_VALID_SIGNATURES();
-// error NOT_ENOUGH_SIGNERS();
-// error NOT_OWNER();
-// error NOT_SELF();
-// error NOT_FACTORY();
-// error TX_FAILED();
+//custom errors
+error DUPLICATE_OR_UNORDERED_SIGNATURES();
+error INVALID_OWNER();
+error INVALID_SIGNER();
+error INVALID_SIGNATURES_REQUIRED();
+error INSUFFICIENT_VALID_SIGNATURES();
+error NOT_ENOUGH_SIGNERS();
+error NOT_OWNER();
+error NOT_SELF();
+error NOT_FACTORY();
+error TX_FAILED();
 
-// contract MultiSigWalletTest {
-//     using ECDSA for bytes32;
-//     MultiSigFactory private multiSigFactory;
-//     uint256 public constant factoryVersion = 1; // <---- set the factory version for backword compatiblity for future contract updates
-//     bool useOracle;
+contract MultiSigWalletTest {
+    using ECDSA for bytes32;
+    MultiSigFactory private multiSigFactory;
+    uint256 public constant factoryVersion = 1; // <---- set the factory version for backword compatiblity for future contract updates
+    bool useOracle;
 
-//     event Deposit(address indexed sender, uint256 amount, uint256 balance);
-//     event ExecuteTransaction(
-//         address indexed owner,
-//         address payable to,
-//         uint256 value,
-//         bytes data,
-//         uint256 nonce,
-//         bytes32 hash,
-//         bytes result
-//     );
-//     event Owner(address indexed owner, bool added);
+    event Deposit(address indexed sender, uint256 amount, uint256 balance);
+    event ExecuteTransaction(
+        address indexed owner,
+        address payable to,
+        uint256 value,
+        bytes data,
+        uint256 nonce,
+        bytes32 hash,
+        bytes result
+    );
+    event Owner(address indexed owner, bool added);
 
-//     mapping(address => bool) public isOwner;
+    mapping(address => bool) public isOwner;
 
-//     address[] public owners;
+    address[] public owners;
 
-//     uint256 public signaturesRequired;
-//     uint256 public nonce;
-//     uint256 public chainId;
-//     string public name;
-//     uint upperThreshold;
-//     uint lowerThreshold;
+    uint256 public signaturesRequired;
+    uint256 public nonce;
+    uint256 public chainId;
+    string public name;
+    uint upperThreshold;
+    uint lowerThreshold;
+    uint withdrawnBalance;
 
-//     modifier onlyOwner() {
-//         if (!isOwner[msg.sender]) {
-//             revert NOT_OWNER();
-//         }
-//         _;
-//     }
+    modifier onlyOwner() {
+        if (!isOwner[msg.sender]) {
+            revert NOT_OWNER();
+        }
+        _;
+    }
 
-//     modifier onlySelf() {
-//         if (msg.sender != address(this)) {
-//             revert NOT_SELF();
-//         }
-//         _;
-//     }
+    modifier onlySelf() {
+        if (msg.sender != address(this)) {
+            revert NOT_SELF();
+        }
+        _;
+    }
 
-//     modifier onlyValidSignaturesRequired() {
-//         _;
-//         if (signaturesRequired == 0) {
-//             revert INVALID_SIGNATURES_REQUIRED();
-//         }
-//         if (owners.length < signaturesRequired) {
-//             revert NOT_ENOUGH_SIGNERS();
-//         }
-//     }
-//     modifier onlyFactory() {
-//         if (msg.sender != address(multiSigFactory)) {
-//             revert NOT_FACTORY();
-//         }
-//         _;
-//     }
+    modifier onlyValidSignaturesRequired() {
+        _;
+        if (signaturesRequired == 0) {
+            revert INVALID_SIGNATURES_REQUIRED();
+        }
+        if (owners.length < signaturesRequired) {
+            revert NOT_ENOUGH_SIGNERS();
+        }
+    }
+    modifier onlyFactory() {
+        if (msg.sender != address(multiSigFactory)) {
+            revert NOT_FACTORY();
+        }
+        _;
+    }
 
-//     constructor(
-//         string memory _name,
-//         address _factory,
-//         uint _upperThreshold,
-//         uint _lowerThreshold
-//     ) payable {
-//         name = _name;
-//         multiSigFactory = MultiSigFactory(_factory);
-//         upperThreshold = _upperThreshold;
-//         lowerThreshold = _lowerThreshold;
-//     }
+    constructor(
+        string memory _name,
+        address _factory,
+        uint _withdrawnBalance,
+        uint _upperThreshold,
+        uint _lowerThreshold
+    ) payable {
+        name = _name;
+        multiSigFactory = MultiSigFactory(_factory);
+        upperThreshold = _upperThreshold;
+        lowerThreshold = _lowerThreshold;
+        withdrawnBalance = _withdrawnBalance;
+    }
 
-//     function init(
-//         uint256 _chainId,
-//         address[] calldata _owners,
-//         uint256 _signaturesRequired,
-//         bool _useOracle
-//     ) public payable onlyFactory onlyValidSignaturesRequired {
-//         signaturesRequired = _signaturesRequired;
-//         useOracle = _useOracle;
-//         if (useOracle) {
-//             callOracle();
-//         }
-//         for (uint256 i = 0; i < _owners.length; ) {
-//             address owner = _owners[i];
-//             if (owner == address(0) || isOwner[owner]) {
-//                 revert INVALID_OWNER();
-//             }
-//             isOwner[owner] = true;
-//             owners.push(owner);
+    function init(
+        uint256 _chainId,
+        address[] calldata _owners,
+        uint256 _signaturesRequired,
+        bool _useOracle
+    ) public payable onlyFactory onlyValidSignaturesRequired {
+        signaturesRequired = _signaturesRequired;
+        useOracle = _useOracle;
+        if (useOracle) {
+            callOracle();
+        }
+        for (uint256 i = 0; i < _owners.length; ) {
+            address owner = _owners[i];
+            if (owner == address(0) || isOwner[owner]) {
+                revert INVALID_OWNER();
+            }
+            isOwner[owner] = true;
+            owners.push(owner);
 
-//             emit Owner(owner, isOwner[owner]);
-//             unchecked {
-//                 ++i;
-//             }
-//         }
+            emit Owner(owner, isOwner[owner]);
+            unchecked {
+                ++i;
+            }
+        }
 
-//         chainId = _chainId;
-//     }
+        chainId = _chainId;
+    }
 
-//     function callOracle() public onlySelf {
-//         Oracle oracle = new Oracle();
-//     }
+    function callOracle() public onlySelf {
+        Oracle oracle = new Oracle();
+    }
 
-//     function checkOracle() public view returns (bool) {
-//         return useOracle;
-//     }
+    function checkOracle() public view returns (bool) {
+        return useOracle;
+    }
 
-//     function addSigner(address newSigner, uint256 newSignaturesRequired)
-//         public
-//         onlySelf
-//         onlyValidSignaturesRequired
-//     {
-//         if (newSigner == address(0) || isOwner[newSigner]) {
-//             revert INVALID_SIGNER();
-//         }
+    function addSigner(address newSigner, uint256 newSignaturesRequired)
+        public
+        onlySelf
+        onlyValidSignaturesRequired
+    {
+        if (newSigner == address(0) || isOwner[newSigner]) {
+            revert INVALID_SIGNER();
+        }
 
-//         isOwner[newSigner] = true;
-//         owners.push(newSigner);
-//         signaturesRequired = newSignaturesRequired;
+        isOwner[newSigner] = true;
+        owners.push(newSigner);
+        signaturesRequired = newSignaturesRequired;
 
-//         emit Owner(newSigner, isOwner[newSigner]);
-//         multiSigFactory.emitOwners(
-//             address(this),
-//             owners,
-//             newSignaturesRequired
-//         );
-//     }
+        emit Owner(newSigner, isOwner[newSigner]);
+        multiSigFactory.emitOwners(
+            address(this),
+            owners,
+            newSignaturesRequired
+        );
+    }
 
-//     function removeSigner(address oldSigner, uint256 newSignaturesRequired)
-//         public
-//         onlySelf
-//         onlyValidSignaturesRequired
-//     {
-//         if (!isOwner[oldSigner]) {
-//             revert NOT_OWNER();
-//         }
+    function removeSigner(address oldSigner, uint256 newSignaturesRequired)
+        public
+        onlySelf
+        onlyValidSignaturesRequired
+    {
+        if (!isOwner[oldSigner]) {
+            revert NOT_OWNER();
+        }
 
-//         _removeOwner(oldSigner);
-//         signaturesRequired = newSignaturesRequired;
+        _removeOwner(oldSigner);
+        signaturesRequired = newSignaturesRequired;
 
-//         emit Owner(oldSigner, isOwner[oldSigner]);
-//         multiSigFactory.emitOwners(
-//             address(this),
-//             owners,
-//             newSignaturesRequired
-//         );
-//     }
+        emit Owner(oldSigner, isOwner[oldSigner]);
+        multiSigFactory.emitOwners(
+            address(this),
+            owners,
+            newSignaturesRequired
+        );
+    }
 
-//     function _removeOwner(address _oldSigner) private {
-//         isOwner[_oldSigner] = false;
-//         uint256 ownersLength = owners.length;
-//         address[] memory poppedOwners = new address[](owners.length);
-//         for (uint256 i = ownersLength - 1; i >= 0; ) {
-//             if (owners[i] != _oldSigner) {
-//                 poppedOwners[i] = owners[i];
-//                 owners.pop();
-//             } else {
-//                 owners.pop();
-//                 for (uint256 j = i; j < ownersLength - 1; ) {
-//                     owners.push(poppedOwners[j + 1]);
-//                     unchecked {
-//                         ++j;
-//                     }
-//                 }
-//                 return;
-//             }
-//             unchecked {
-//                 --i;
-//             }
-//         }
-//     }
+    function _removeOwner(address _oldSigner) private {
+        isOwner[_oldSigner] = false;
+        uint256 ownersLength = owners.length;
+        address[] memory poppedOwners = new address[](owners.length);
+        for (uint256 i = ownersLength - 1; i >= 0; ) {
+            if (owners[i] != _oldSigner) {
+                poppedOwners[i] = owners[i];
+                owners.pop();
+            } else {
+                owners.pop();
+                for (uint256 j = i; j < ownersLength - 1; ) {
+                    owners.push(poppedOwners[j + 1]);
+                    unchecked {
+                        ++j;
+                    }
+                }
+                return;
+            }
+            unchecked {
+                --i;
+            }
+        }
+    }
 
-//     function updateSignaturesRequired(uint256 newSignaturesRequired)
-//         public
-//         onlySelf
-//         onlyValidSignaturesRequired
-//     {
-//         signaturesRequired = newSignaturesRequired;
-//     }
+    function updateSignaturesRequired(uint256 newSignaturesRequired)
+        public
+        onlySelf
+        onlyValidSignaturesRequired
+    {
+        signaturesRequired = newSignaturesRequired;
+    }
 
-//     function executeTransaction(
-//         address payable to,
-//         uint256 value,
-//         bytes calldata data,
-//         bytes[] calldata signatures
-//     ) public onlyOwner returns (bytes memory) {
-//         bytes32 _hash = getTransactionHash(nonce, to, value, data);
+    function executeTransaction(
+        address payable to,
+        uint256 value,
+        bytes calldata data,
+        bytes[] calldata signatures
+    ) public onlyOwner returns (bytes memory) {
+        bytes32 _hash = getTransactionHash(nonce, to, value, data);
 
-//         nonce++;
+        nonce++;
 
-//         uint256 validSignatures;
-//         address duplicateGuard;
-//         for (uint256 i = 0; i < signatures.length; ) {
-//             address recovered = recover(_hash, signatures[i]);
-//             if (recovered <= duplicateGuard) {
-//                 revert DUPLICATE_OR_UNORDERED_SIGNATURES();
-//             }
-//             duplicateGuard = recovered;
+        uint256 validSignatures;
+        address duplicateGuard;
+        for (uint256 i = 0; i < signatures.length; ) {
+            address recovered = recover(_hash, signatures[i]);
+            if (recovered <= duplicateGuard) {
+                revert DUPLICATE_OR_UNORDERED_SIGNATURES();
+            }
+            duplicateGuard = recovered;
 
-//             if (isOwner[recovered]) {
-//                 validSignatures++;
-//             }
-//             unchecked {
-//                 ++i;
-//             }
-//         }
+            if (isOwner[recovered]) {
+                validSignatures++;
+            }
+            unchecked {
+                ++i;
+            }
+        }
 
-//         if (validSignatures < signaturesRequired) {
-//             revert INSUFFICIENT_VALID_SIGNATURES();
-//         }
+        if (validSignatures < signaturesRequired) {
+            revert INSUFFICIENT_VALID_SIGNATURES();
+        }
 
-//         (bool success, bytes memory result) = to.call{value: value}(data);
-//         if (!success) {
-//             revert TX_FAILED();
-//         }
+        (bool success, bytes memory result) = to.call{value: value}(data);
+        if (!success) {
+            revert TX_FAILED();
+        }
 
-//         emit ExecuteTransaction(
-//             msg.sender,
-//             to,
-//             value,
-//             data,
-//             nonce - 1,
-//             _hash,
-//             result
-//         );
-//         return result;
-//     }
+        emit ExecuteTransaction(
+            msg.sender,
+            to,
+            value,
+            data,
+            nonce - 1,
+            _hash,
+            result
+        );
+        return result;
+    }
 
-//     function getTransactionHash(
-//         uint256 _nonce,
-//         address to,
-//         uint256 value,
-//         bytes calldata data
-//     ) public view returns (bytes32) {
-//         return
-//             keccak256(
-//                 abi.encodePacked(
-//                     address(this),
-//                     chainId,
-//                     _nonce,
-//                     to,
-//                     value,
-//                     data
-//                 )
-//             );
-//     }
+    function getTransactionHash(
+        uint256 _nonce,
+        address to,
+        uint256 value,
+        bytes calldata data
+    ) public view returns (bytes32) {
+        return
+            keccak256(
+                abi.encodePacked(
+                    address(this),
+                    chainId,
+                    _nonce,
+                    to,
+                    value,
+                    data
+                )
+            );
+    }
 
-//     function recover(bytes32 _hash, bytes calldata _signature)
-//         public
-//         pure
-//         returns (address)
-//     {
-//         return _hash.toEthSignedMessageHash().recover(_signature);
-//     }
+    function recover(bytes32 _hash, bytes calldata _signature)
+        public
+        pure
+        returns (address)
+    {
+        return _hash.toEthSignedMessageHash().recover(_signature);
+    }
 
-//     function checkUpkeep(
-//         bytes calldata /* checkData */
-//     )
-//         external
-//         view
-//         override
-//         returns (
-//             bool upkeepNeeded,
-//             bytes memory /* performData */
-//         )
-//     {
-//         upkeepNeeded = checkETHPrice(upperThreshold, lowerThreshold);
-//         // To get rid of the warning
-//         return (upkeepNeeded, "0x0");
-//         // We don't use the checkData in this example. The checkData is defined when the Upkeep was registered.
-//     }
+    function checkUpkeep(
+        bytes calldata /* checkData */
+    )
+        external
+        view
+        override
+        returns (
+            bool upkeepNeeded,
+            bytes memory /* performData */
+        )
+    {
+        upkeepNeeded = checkETHPrice(upperThreshold, lowerThreshold);
+        // To get rid of the warning
+        return (upkeepNeeded, "0x0");
+        // We don't use the checkData in this example. The checkData is defined when the Upkeep was registered.
+    }
 
-//     function performUpkeep(
-//         bytes calldata /* performData */
-//     ) external override {
-//         //We highly recommend revalidating the upkeep in the performUpkeep function
-//         (bool upkeepNeeded, ) = checkUpkeep("");
-//         require(upkeepNeeded, "Upkeep not needed");
-//         // We don't use the performData in this example. The performData is generated by the Keeper's call to your checkUpkeep function
-//     }
+    function performUpkeep(
+        bytes calldata /* performData */
+    ) external override {
+        //We highly recommend revalidating the upkeep in the performUpkeep function
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        require(upkeepNeeded, "Upkeep not needed");
+        // 1. Add 1 signature
+        // updateSignaturesRequired(signaturesRequired - 1);
+        // 2. Set signature needed to 1
+        // updateSignaturesRequired(1);
+        // 3. Automatically distribute funds equally to recipients address (owners) if funds are over withdraw balance
+        if (balance(address(this)) >= withdrawnBalance) {
+            float dividends = balance(address(this)/owners.length;
+            for(int i = 0; i <owners.length; i++){
+                (bool success, ) = address(this).call{value: dividends};
+                require(success, "transfer failed");
+            }
+        }
+    }
 
-//     receive() external payable {
-//         emit Deposit(msg.sender, msg.value, address(this).balance);
-//     }
+    receive() external payable {
+        emit Deposit(msg.sender, msg.value, address(this).balance);
+    }
 
-//     function numberOfOwners() public view returns (uint256) {
-//         return owners.length;
-//     }
-// }
+    function numberOfOwners() public view returns (uint256) {
+        return owners.length;
+    }
+}
